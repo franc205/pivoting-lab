183b1ec4ddd8e32f28fb62044bec5a662927580c
Francisco Canteli <franc.c205@gmail.com>
2023-10-30 23:51:05 -0300

    Hot Fix Before Workshop

--- configs/files/start-attacker.sh
+++ configs/files/start-attacker.sh
@@ -3,6 +3,6 @@ Xvfb :1 -screen 0 1600x900x16 &
 DISPLAY=:1.0 startxfce4 &
 x11vnc -forever -usepw -display :1.0 -geometry 1600x900 &
 websockify -D --web=/usr/share/novnc/ 6080 localhost:5900 &
-chmod +x /root/Desktop/Walkthrough.desktop &
 /usr/sbin/sshd -D &
+chmod +x /root/Desktop/Walkthrough.desktop &
 tail -f /dev/null
