8cdc90e70aaf47f8414a3522f6d6916c3d4fd880
Francisco Canteli <franc.c205@gmail.com>
2024-06-12 21:16:09 -0300

    More Updates

--- configs/files/start-attacker.sh
+++ configs/files/start-attacker.sh
@@ -4,5 +4,6 @@ DISPLAY=:1.0 startxfce4 &
 x11vnc -forever -usepw -display :1.0 -geometry 1600x900 &
 websockify -D --web=/usr/share/novnc/ 6080 localhost:5900 &
 /usr/sbin/sshd -D &
-chmod +x /root/Desktop/Walkthrough.desktop &
+chmod +x /root/Desktop/Walkthrough_EN &
+chmod +x /root/Desktop/Walkthrough_ES &
 tail -f /dev/null
