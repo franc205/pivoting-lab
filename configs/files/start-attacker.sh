cabbc7cf78104f6e8ebfdc78e3ba2ff941bc43c8
Francisco Canteli <franc.c205@gmail.com>
2025-11-07 16:59:14 -0300

    Fixed noVNC

--- configs/files/start-attacker.sh
+++ configs/files/start-attacker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 Xvfb :1 -screen 0 1600x900x16 &
 DISPLAY=:1.0 startxfce4 &
-x11vnc -forever -usepw -display :1.0 -geometry 1600x900 &
+#x11vnc -forever -usepw -display :1.0 -geometry 1600x900 &
+x11vnc -display ":1" -rfbport 5900 -rfbauth /root/.vnc/passwd -forever -shared -o /dev/stdout &
 websockify -D --web=/usr/share/novnc/ 6080 localhost:5900 &
 /usr/sbin/sshd -D &
 chmod +x /root/Desktop/Walkthrough_EN.desktop &
 chmod +x /root/Desktop/Walkthrough_ES.desktop  &
 tail -f /dev/null
+
+
